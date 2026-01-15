bb004d04761f48eb9bc9be0f50b74359e91693c9
ErinHaywardLang <erinhayward-lang@hotmail.co.uk>
2026-01-15 22:58:24 +0000

    Fix height

--- js/monthly-playlists.js
+++ js/monthly-playlists.js
@@ -5,9 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
     yearHeaders.forEach(header => {
         header.addEventListener('click', function() {
             const yearSection = this.parentElement;
+            const yearContent = yearSection.querySelector('.year-content');
             
             // Toggle the expanded class
             yearSection.classList.toggle('expanded');
+            
+            // If expanding, set max-height to content height
+            if (yearSection.classList.contains('expanded')) {
+                yearContent.style.maxHeight = yearContent.scrollHeight + 'px';
+            } else {
+                yearContent.style.maxHeight = '0px';
+            }
         });
     });
 });
+
